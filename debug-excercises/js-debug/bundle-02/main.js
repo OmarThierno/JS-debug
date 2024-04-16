401f6bff28c8426b72a676ea4ff30bc6829dcabe
OmarThierno <thiernomarndiaye76@gmail.com>
2024-04-16 15:33:44 +0200

    feat: Bundle 02

--- debug-excercises/js-debug/bundle-02/main.js
+++ debug-excercises/js-debug/bundle-02/main.js
@@ -9,47 +9,69 @@
 ​
 *******************************************************************************/
 
-
+console.log('Es 1--------------');
 // ESERCIZIO 1
-function checkAge() {
-    const myAge = 32;
-    const message = '';
+const myAge = 32;
 
-    if (myAge < 18) {
-        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+function checkAge(age) {
+    let message = '';
+
+    if (age < 18) {
+        message = `Sei troppo giovane! Hai ${age} anni!`;
+        return message
     } else {
         message = 'Hai più di 18 anni!';
+        return message
     }
 }
-checkAge();
+console.log(checkAge(myAge));
+
+// message deve essere dichiarata con let e non con Const
+// MyAge deve essere dichiarata fuori dalla funzione 
+
+console.log('/Es 1--------------');
+
+
+console.log('Es 2--------------');
 
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
 }
 printColorsNumber();
 
+// errore di sintassi, length si scrive cosi 
+console.log('/Es 2--------------');
+
+
+console.log('Es 3--------------');
 
 // ESERCIZIO 3
 function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
+    const userNumber = parseInt(prompt('Inserisci un numero'));
     const total = userNumber + 12;
 
     console.log(`Il risultato finale è ${total}`);
 }
-addNumbers();
+// addNumbers();
 
+// dobbiamo aggiungere il parseInt cosi da trasformare da Stinga a numero quello che utente ci forisce con il prompt, senno potrebbe essere che JS lo interpreti come una concatenazione
+
+console.log('/Es 3--------------');
+
+
+console.log('Es 4--------------');
 
 // ESERCIZIO 4
 function checkAccess() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+    let grantAccess = false;
 
     if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
+        grantAccess = true;
     }
 
     if (grantAccess === true) {
@@ -58,8 +80,14 @@ function checkAccess() {
         console.log('Accesso negato!');
     }
 }
-checkAccess();
+// checkAccess();
 
+// il valore booleano di grantAccess non deve essere racchiuso tra appici (almeno in questo esercizio)
+
+console.log('/Es 3--------------');
+
+
+console.log('Es 4--------------');
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
 function checkAccessImproved() {
@@ -67,29 +95,31 @@ function checkAccessImproved() {
 
     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+    let grantAccess = false;
 
     for (let i = 0; i < addresses.length; i++) {
         const email = addresses[i];
 
-        if (userEmail.length > 5) {
-
-            if (email === userEmail) {
-                grantAccess = 'true';
-
-            }
-
-        }
-
-        if (grantAccess) {
-            console.log('Accesso consentito!');
-        } else {
-            console.log('Accesso negato!');
+        if (email === userEmail) {
+            grantAccess = true;
         }
     }
-    checkAccessImproved();
+    if (grantAccess) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+    
+}
 
+checkAccessImproved();
 
+// mancava la chiusura della funzione
+// la condizione userEmail.length > 5 è in più
+// il valore booleano di grantAccess non deve essere racchiuso tra appici (almeno in questo esercizio)
+//la condizione is grantAccess deve essere fuori dal ciclo for
+
+console.log('/Es 4--------------');
 
 
 
